269d0335a8a79fb0dd1046c320006ae13b68d45d
LiaSkarllate <liaskarllate@protonmail.com>
2021-07-30 20:34:20 -0300

    Added 'Using-Vue' and 'Using-Vue-with-CLI' projects.

---
+++ Using-Vue-with-CLI/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <Comments/>
+</template>
+
+<script>
+    //Imports:
+    import Comments from './components/Comments'
+
+    export default {
+        name: 'app',
+        components: {
+            Comments
+        }
+    }
+</script>
