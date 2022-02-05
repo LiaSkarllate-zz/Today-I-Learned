a4f73dab101fed924a168811e296c8d1fa406678
LiaSkarllate <liaskarllate@protonmail.com>
2022-02-05 15:32:18 -0300

    fix: Project 'Using-MySQL-with-JDBC' renamed.

--- Using-MySQL-with-JDBC/src/main/java/br/unesp/rpg/sql/IMySQL.java
+++
@@ -1,9 +0,0 @@
-package br.unesp.rpg.sql;
-
-public interface IMySQL {
-    //Constants:
-    final String USER = "";
-    final String PASSWORD = "";
-    final String URL = "jdbc:mysql://localhost:3306/using-jdbc?useTimezone=true&serverTimezone=UTC";
-    final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
-}
